5a2dc9872a690749c69a1861d848da36546d8b3c
Prince Sharma <princesharmayuvraj204@gmail.com>
2026-07-15 21:43:17 +0530

    Update bill.js

--- bill.js
+++ bill.js
@@ -1,6 +1,6 @@
 let mainFrame = document.getElementById("main-frame"); 
 mainFrame.className = "border-2 border-black flex justify-center items-start gap-12 min-h-screen bg-gray-100 p-10";
-
+ 
 let amountSection = document.getElementById("amount-section");
 amountSection.className ="w-[420px] rounded-lg p-6 flex flex-col gap-5"; 
  
